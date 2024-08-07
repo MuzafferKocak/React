d18531873b1ee0bd81ff6c9173100ca374b165c0
Mahir <mkojak75@gmail.com>
2024-08-07 16:17:46 +0200

    ternary

--- 02_comp_Jsx/props/src/App.js
+++ 02_comp_Jsx/props/src/App.js
@@ -9,9 +9,20 @@ function App() {
       name="Cenitin" 
       img="https://cdn.pixabay.com/photo/2019/10/18/09/29/cat-4558651_1280.jpg"
       color="grey"
+      isBlueEyed={true}
       />
-      <Cat/>
-      <Cat/>
+      <Cat 
+        name="Garfield" 
+      img="https://cdn.pixabay.com/photo/2014/03/29/09/17/cat-300572_1280.jpg"
+      color="orange"
+      isBlueEyed={false}
+      />
+      <Cat 
+        name="Sammy" 
+      img="https://cdn.pixabay.com/photo/2018/02/21/05/17/cat-3169476_1280.jpg"
+      isBlueEyed={false}
+      
+      />
     </div>
   );
 }
--- 02_comp_Jsx/props/src/components/Cat.jsx
+++ 02_comp_Jsx/props/src/components/Cat.jsx
@@ -1,36 +1,36 @@
-
 //* React import ist nicht mehr erforderlich
 // import React from "react"
 
 //* Named export in geschweifte klammern, gleicher Name
-import { Name } from "./Name"
+import { Name } from "./Name";
 
 //? components werden function dec. || arrow func. erstellen
 
-const Cat =({name, img, color})=>{
-    //? destraction
-    // const {name,img,color}= props
-    return(
-        //* JSX ausdrücke wenn mehr als ein ausdruck ist muss dieser von einem übergeordneten Ausdruck umgeben sein.
-        <div>
-        <Name name={name}/>
-        
-        <img src={img} alt="cat-img" height="300px" />
-        <p style={{backgroundColor: color}}>Color: {color}</p>
-        <hr />
+const Cat = ({ name, img, color = "grey", isBlueEyed }) => {
+  //? destraction
+  // const {name,img,color}= props
+  return (
+    //* JSX ausdrücke wenn mehr als ein ausdruck ist muss dieser von einem übergeordneten Ausdruck umgeben sein.
+    <div>
+      <Name name={name} />
+      {/* //* In JSX können wir kein if/else benutzen nur Ternary benutzen */}
+      <p>Blue Eyed ? : {isBlueEyed ? "Yes" : "No"}</p>
 
-        </div>
-    )
-}
+      <img src={img} alt="cat-img" height="300px" />
+      <p style={{ backgroundColor: color }}>Color: {color}</p>
+      <hr />
+    </div>
+  );
+};
 
-export default Cat
+export default Cat;
 
 // const Cat =(props)=>{
 //     return(
 //         //* JSX ausdrücke wenn mehr als ein ausdruck ist muss dieser von einem übergeordneten Ausdruck umgeben sein.
 //         <div>
 //         <Name/>
-        
+
 //         <img src={props.img} alt="cat-img" height="300px" />
 //         <p style={{backgroundColor: props.color}}>Color: {props.color}</p>
 //         <hr />
