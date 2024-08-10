6a01b59e38592eb332435b79d6511367c04d209f
Mahir <mkojak75@gmail.com>
2024-08-10 22:53:04 +0200

    sass-styling

--- 03_styling/sass-styling/src/App.js
+++ 03_styling/sass-styling/src/App.js
@@ -1,13 +1,16 @@
+import Card from "./components/Card";
 import Header from "./components/Header";
 //? SCSS stylesheet import
-import "./sass/app.scss"
-
-
+import "./sass/app.scss";
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      {data.map(({ id, name, job, img, comment }) => (
+        <Card key={id} name={name} job={job} img={img} comment={comment} />
+      ))}
     </div>
   );
 }
--- 03_styling/sass-styling/src/components/Card.jsx
+++ 03_styling/sass-styling/src/components/Card.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from "react";
+import Cardstyle from "../sass/card.module.scss";
 
-const Card = ({name, job, comment, img}) => {
+const Card = ({ name, job, comment, img }) => {
+    
   return (
-    <div className='container'>
-    <div className='card'>
-    <h1>{name}</h1>
-    <h3>{job}</h3>
-    <p>{comment}</p>
-    <img src={img} alt={name} />
-    <div className='buttons'>
-    <button className='small'>Small</button>
-    <button className='large'>Large</button>
-
+    <div className="container">
+      <div className={Cardstyle["card"]}>
+        <h1>{name}</h1>
+        <h3>{job}</h3>
+        <p>{comment}</p>
+        <img src={img} alt={name} />
+        <div className={Cardstyle["buttons"]}>
+          <button className={Cardstyle["small"]}>Small</button>
+          <button className={Cardstyle["large"]}>Large</button>
+        </div>
+      </div>
     </div>
+  );
+};
 
-
-    </div>
-
-    </div>
-  )
-}
-
-export default Card
+export default Card;
