995cde46477a8d4f7b8d2dd71d51f88a23c6786d
Mahir <mkojak75@gmail.com>
2024-08-18 00:18:50 +0200

    toggle

--- 05_events-states/src/3-useStateComp/UseStateObject.jsx
+++ 05_events-states/src/3-useStateComp/UseStateObject.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
+  //? State definition
+  // const [name, setName]= useState("Max")
+  // const [nachName, setNachname]= useState("Müller")
+  // const [beruf, setBeruf]= useState("Developer")
+  // const [lohn, setLohn]= useState(2500)
+
+  const [person, setPerson]= useState({
+    name: "Max",
+    nachName: "Müller",
+    beruf: "Developer",
+    lohn: 2500,
+    stimmung: "Glücklich",
+  })
+  console.log(person);
+
+  const [toggle, setToggle] = useState(true)
+
+  const handleStimmung=()=>{
+    setPerson({...person, stimmung: toggle ? "Traurig" : "Glücklich"})
+    setToggle(!toggle)
+  }
+
+  // const NameWechsel = ()=>{
+  //   setName("Martin")
+  // }
+
+
+
   return (
     <div>
         <h1>UseState Object Example</h1>
-        <h2>NAME: Name</h2>
-        <h2>NACHNAME: Nachname</h2>
-        <h2>BERUF: Beruf</h2>
-        <h2>LOHN: Lohn</h2>
+        <h2>NAME: {person.name}</h2>
+        <h2>NACHNAME: {person.nachName}</h2>
+        <h2>BERUF: {person.beruf}</h2>
+        <h2>LOHN: {person.lohn}</h2>
 
-        <button>Name wechseln</button>
-        <button>Nachname wechseln</button>
-        <button>Beruf wechseln</button>
-        <button>Lohn erhöhen </button>
-        <button>Lohn reduzieren </button>
+        <h3>Stimmung: {person.stimmung} </h3>
+
+        <button onClick={()=> setPerson({...person, name: "Martin"})}>Name wechseln</button>
+        <button onClick={()=> setPerson({...person,nachName: "Mustermann"})}>Nachname wechseln</button>
+        <button onClick={()=> setPerson({...person, beruf: "Tester"})}>Beruf wechseln</button>
+        <button onClick={()=> setPerson({...person, lohn: person.lohn + 1000})}>Lohn erhöhen </button>
+        <button onClick={()=> setPerson({...person, lohn: person.lohn - 1000})}>Lohn reduzieren </button>
+        <button onClick={handleStimmung}>Glücklich/Traurig</button>
     </div>
   )
 }
