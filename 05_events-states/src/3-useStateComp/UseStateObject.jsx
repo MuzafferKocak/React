d348cd7772edeb51b7afb739d26e63333cfb4a00
Mahir <mkojak75@gmail.com>
2024-08-17 23:16:12 +0200

    example

---
+++ 05_events-states/src/3-useStateComp/UseStateObject.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const UseStateObject = () => {
+  return (
+    <div>
+        <h1>UseState Object Example</h1>
+        <h2>NAME: Name</h2>
+        <h2>NACHNAME: Nachname</h2>
+        <h2>BERUF: Beruf</h2>
+        <h2>LOHN: Lohn</h2>
+
+        <button>Name wechseln</button>
+        <button>Nachname wechseln</button>
+        <button>Beruf wechseln</button>
+        <button>Lohn erhöhen </button>
+        <button>Lohn reduzieren </button>
+    </div>
+  )
+}
+
+export default UseStateObject
