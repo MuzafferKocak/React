5e0f58f79fd4bbc3e2330c89f2b4906d5254a888
Mahir <mkojak75@gmail.com>
2024-08-19 23:57:59 +0200

    keydown

--- 06_forms-events/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ 06_forms-events/src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import { useState } from 'react'
 
 const KeyboardClipboard = () => {
+
+    const [inputData, setInputData] = useState("")
+    // console.log(inputData);
+    
+    const handleKeyDown = (e) => {
+        console.log(e.keyCode)
+        if (e.keyCode > 47 && e.keyCode < 58) {
+          alert("a number pressed")
+          e.preventDefault()
+        }
+      }
+    
+
   return (
     <div className='container mt-4'>
         <h2>Keyboard Events</h2>
 
 
-        <input type="text" className='form-control' />
+        <input 
+        type="text" 
+        className='form-control'
+        onChange={(e)=> setInputData(e.target.value.toLocaleUpperCase())}
+        value={inputData}
+        onKeyDown={handleKeyDown}
+         />
 
         <div className='mt-3'>
-            <p>Schrift</p>
+            <p>{inputData.toLocaleLowerCase()}</p>
         </div>
 
         <div>
