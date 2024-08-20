1c125f4f5dfad1813bf6c04ec70408a3d8567522
Mahir <mkojak75@gmail.com>
2024-08-21 00:01:05 +0200

    lifeCycle

---
+++ 07_lifecycle-useffect/src/App.js
@@ -0,0 +1,13 @@
+
+
+import LifeCycleMethods from './components/LifeCycleMethods';
+
+function App() {
+  return (
+    <div className="container text-center mt-4">
+      <LifeCycleMethods/>
+    </div>
+  );
+}
+
+export default App;
