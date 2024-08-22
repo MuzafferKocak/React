e9486fc6b8097b52fdbca507a02fdd9ade3b78a3
Mahir <mkojak75@gmail.com>
2024-08-23 00:31:37 +0200

    user-example

--- 07_lifecycle-useffect/src/components/User.jsx
+++ 07_lifecycle-useffect/src/components/User.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const User = () => {
     const [userData, setUserData] = useState({
@@ -15,19 +15,25 @@ const User = () => {
         const res = await fetch("https://randomuser.me/api/")
         const data = await res.json()
         console.log(data.results[0]);
+        setUserData(data.results[0])
 
     }
-
-    getUser()
+    //? componentDidMount
+    useEffect(() => {
+      getUser()
+    }, [])
+    
+    console.log(userData);
+    // getUser() //* endlosschleife, wenn mann so ausruft!
   return (
-    <div>
-        <img src="" alt="" />
+    <div className=''>
+        <img className='rounded-circle m-4' src={userData.picture.large} alt="" />
         <p>Hi My Name is</p>
-        <h2>name surname</h2>
-        <h4>email</h4>
-        <h4>date</h4>
-        <h4>city</h4>
-        <h4>telephone</h4>
+        <h2>{userData?.name?.first + " " + userData.name.last }</h2>
+        <h4>{userData.email}</h4>
+        <h5>{userData.dob.age}</h5>
+        <h4>{userData.location.city}</h4>
+        <h6>{userData.cell}</h6>
 
         <button className='btn btn-warning' onClick={getUser}>new user</button>
     </div>
