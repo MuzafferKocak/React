c4dedf7653f4bbc0fb834a35a5ec3977b5cad0a1
Mahir <mkojak75@gmail.com>
2024-08-24 23:27:07 +0200

    axios

--- 08_tutorial-app/src/pages/Home.jsx
+++ 08_tutorial-app/src/pages/Home.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import AddTutorial from '../components/AddTutorial'
-import TutorialList from '../components/TutorialList'
+import React, { useEffect, useState } from "react";
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+import axios from "axios";
 
 const Home = () => {
+
+    const [tutorials, setTutorials] = useState([])
+
+
+  const getTutorials = async () => {
+    const URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+    const res = await axios(URL);
+    console.log(res.data);
+    setTutorials(res.data)
+  };
+
+  //? componentDidMount (nach dem ersten rendern, einmal Anfrage senden)
+  useEffect(()=>{
+    getTutorials()
+  }, [])
+
+//   getTutorials()
+
   return (
     <>
-    <AddTutorial/>
-    <TutorialList/>
-
-
+      <AddTutorial />
+      <TutorialList />
     </>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
