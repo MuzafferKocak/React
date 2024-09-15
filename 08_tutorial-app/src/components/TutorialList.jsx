973b01cc1e1b1aa788d7deadf57533c539063cc3
Mahir <mkojak75@gmail.com>
2024-09-15 20:08:31 +0200

    modal

--- 08_tutorial-app/src/components/TutorialList.jsx
+++ 08_tutorial-app/src/components/TutorialList.jsx
@@ -1,6 +1,7 @@
 import { FaEdit } from "react-icons/fa"
 import { AiFillDelete } from "react-icons/ai"
 import axios from "axios"
+import EditModal from "./EditModal"
 
 const TutorialList = ({tutorials, getTutorials}) => {
 
@@ -72,6 +73,7 @@ const TutorialList = ({tutorials, getTutorials}) => {
           })}
         </tbody>
       </table>
+      <EditModal/>
     </div>
   )
 }
