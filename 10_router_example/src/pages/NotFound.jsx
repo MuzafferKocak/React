b499b73bd6865eb3a786b0a8250ea3d4d70921f6
Mahir <mkojak75@gmail.com>
2024-09-19 21:19:36 +0200

    spinner

--- 10_router_example/src/pages/NotFound.jsx
+++ 10_router_example/src/pages/NotFound.jsx
@@ -1,12 +1,16 @@
 import errorImg from "../img/404.png";
+import { useNavigate } from "react-router-dom";
+
+
 
 const NotFound = () => {
+  const navigate = useNavigate()
   return (
     <div className="container text-center mt-4">
       <img className="w-75" src={errorImg} alt="" />
       <div>
-        <button className="btn btn-success me-2">Home</button>
-        <button className="btn btn-warning">Go Back</button>
+        <button className="btn btn-success me-2" onClick={()=> navigate("/")}>Home</button>
+        <button className="btn btn-warning" onClick={()=> navigate(-1)}>Go Back</button>
       </div>
     </div>
   );
--- 10_router_example/src/pages/PersonDetail.jsx
+++ 10_router_example/src/pages/PersonDetail.jsx
@@ -1,35 +1,51 @@
 import { useState, useEffect } from "react";
 import {  useParams, useNavigate } from "react-router-dom";
 import NotFound from "./NotFound";
+import spinner from "../img/Spinner-2.gif"
 
 const PersonDetail = () => {
   const [person, setPerson] = useState([]);
   const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
   const {id} = useParams()
   const navigate = useNavigate()
 
   
-
+  console.log(id)
   const getPerson = () => {
     fetch(`https://reqres.in/api/users/${id}`)
       .then((res) => {
         if (!res.ok){
             setError(true)
+            setLoading(true)
             throw new Error("User can not be found")
         }
         return res.json()
 
-      } )
-      .then((data) => setPerson(data.data))
-      .catch((err) => console.log(err));
+      })
+      .then((data) => {
+        setLoading(false)
+        setPerson(data.data)
+      })
+      .catch((err) => console.log(err))
   };
   useEffect(() => {
     getPerson();
   }, []);  
 
-  if(error){
-    return <NotFound/>
-} else {
+
+  //? >Conditional Rendering
+
+  if (loading) {
+    return (
+      <div className="text-center">
+        <img src={spinner} alt="" />
+      </div>
+    )
+  } else if (error) {
+    return <NotFound />
+  } else {
     return (
     
         <div className="container text-center mt-4">
@@ -41,7 +57,7 @@ const PersonDetail = () => {
             </h6>
             <p>{person?.email}</p>
             <div className="mt-3">
-                <button className="btn btn-danger " onClick={()=> navigate(-1)}>Back</button>
+                <button className="btn btn-danger me-2 " onClick={()=> navigate(-1)}>Back</button>
                 <button className="btn btn-success" onClick={()=> navigate("/")}>Home</button>
             </div>
           </div>
