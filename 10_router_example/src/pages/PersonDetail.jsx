1aa3998e1a51f8c1c786a5033b3e33e7c92a25f9
Mahir <mkojak75@gmail.com>
2024-09-19 20:45:24 +0200

    notFound

--- 10_router_example/src/pages/PersonDetail.jsx
+++ 10_router_example/src/pages/PersonDetail.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import {  useParams, useNavigate } from "react-router-dom";
+import NotFound from "./NotFound";
 
 const PersonDetail = () => {
   const [person, setPerson] = useState([]);
+  const [error, setError] = useState(false)
   const {id} = useParams()
   const navigate = useNavigate()
 
@@ -10,30 +12,43 @@ const PersonDetail = () => {
 
   const getPerson = () => {
     fetch(`https://reqres.in/api/users/${id}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok){
+            setError(true)
+            throw new Error("User can not be found")
+        }
+        return res.json()
+
+      } )
       .then((data) => setPerson(data.data))
       .catch((err) => console.log(err));
   };
   useEffect(() => {
     getPerson();
-  }, []);
+  }, []);  
 
-  return (
-    <div className="container text-center mt-4">
-      <h1>Person Detail</h1>
-      <img className="rounded" src={person?.avatar} alt="img" />
-      <div className="row justify-content-center g-3">
-        <h6>
-          {person?.first_name} {person?.last_name}
-        </h6>
-        <p>{person?.email}</p>
-        <div className="g-2">
-            <button className="btn btn-danger" onClick={()=> navigate(-1)}>Back</button>
-            <button className="btn btn-success" onClick={()=> navigate("/")}>Home</button>
+  if(error){
+    return <NotFound/>
+} else {
+    return (
+    
+        <div className="container text-center mt-4">
+          <h1>Person Detail</h1>
+          <img className="rounded" src={person?.avatar} alt="img" />
+          <div className="row justify-content-center g-3">
+            <h6>
+              {person?.first_name} {person?.last_name}
+            </h6>
+            <p>{person?.email}</p>
+            <div className="mt-3">
+                <button className="btn btn-danger " onClick={()=> navigate(-1)}>Back</button>
+                <button className="btn btn-success" onClick={()=> navigate("/")}>Home</button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      );
+}
+  
 };
 
 export default PersonDetail;
