c84312b5a4748ffdc7a62a962795694bff156983
Mahir <mkojak75@gmail.com>
2024-09-22 22:05:29 +0200

    update

--- 10_router_example/src/pages/PrivateRouter.jsx
+++ 10_router_example/src/pages/PrivateRouter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
-import Login from './Login'
+
 
 const PrivateRouter = () => {
     //? Context, Redux, Mobx, Zustang (Global State managment, Local Staroge, session)
