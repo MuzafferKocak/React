1a6e2897bfe59a64e95f4b6f40ec19c4f3926816
Mahir <mkojak75@gmail.com>
2024-09-23 16:27:05 +0200

    line-clamp

--- 11_tailwind/src/App.js
+++ 11_tailwind/src/App.js
@@ -1,3 +1,4 @@
+import Typografy from "./components/Typografy";
 
 
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
     <h1 className="text-center text-2xl text-purple-700">Hello Tailwind</h1>
+    <Typografy/>
       
     </div>
   );
--- 11_tailwind/src/components/Typografy.jsx
+++ 11_tailwind/src/components/Typografy.jsx
@@ -4,7 +4,27 @@ const Typografy = () => {
   return (
     <div>
     {/* //* Typografy */}
-    
+    <h3 className='text-2xl '>Typografy</h3>
+    <p className='text-sm '>Typografy</p>
+    <p className='text-lg '>Typografy</p>
+    <p className='text-xl '>Typografy</p>
+    <p className='text-2xl '>Typografy</p>
+    <h2 className='text-3xl '>Typografy</h2>
+    <h3 className='text-4xl '>Typografy</h3>
+    <h3 className='text-[60px] '>Typografy</h3>
+    <h1 className='text-base/8 '>Typografy</h1>
+
+
+    {/* //* Line Clamp */}
+    <article>
+        <p className='line-clamp-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis dolorum doloribus minus, commodi velit est expedita ut accusamus! Natus, rerum earum eveniet nemo incidunt maiores. Tempore asperiores quidem vitae quibusdam.</p>
+    </article>
+
+    <p className='text-center'>Hello</p>
+    <p className='underline'>Hello</p>
+    <p className='line-through'>Hello</p>
+    <p className='uppercase'>Hello</p>
+    <p className='lowercase'>Hello</p>
     </div>
   )
 }
