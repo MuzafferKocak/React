88c26dc2dcf931c775acd3c5d10e6b82a7b438b7
Mahir <mkojak75@gmail.com>
2024-10-04 00:55:27 +0200

    dispatch

--- 13_redux-intro/src/components/counter/Counter.jsx
+++ 13_redux-intro/src/components/counter/Counter.jsx
@@ -1,14 +1,27 @@
+import { useSelector, useDispatch } from "react-redux"
 import "./Counter.css"
 
 const Counter = () => {
+
+
+    //? Der useSelector-Hook wird zum Lesen von Daten aus dem globalen Status verwendet
+
+   const count = useSelector((state)=> state.count)
+
+   const dispatch = useDispatch()
+   console.log(count);
+
+
   return (
     <div className="app">
       <h2 className="counter-header">Counter With Redux</h2>
-      <h1>counter:0</h1>
+      <h1>counter:{count}</h1>
       <div>
-        <button className="counter-button positive">increase</button>
-        <button className="counter-button zero">reset</button>
-        <button className="counter-button negative">decrease</button>
+        <button className="counter-button positive" onClick={()=> dispatch({type: "INC"})}>increase</button>
+
+        <button className="counter-button zero" onClick={()=> dispatch({type: "RESET"})}>reset</button>
+
+        <button className="counter-button negative" onClick={()=> dispatch({type: "DEC"})}>decrease</button>
       </div>
     </div>
   )
--- 13_redux-intro/src/store/counterReducer.jsx
+++ 13_redux-intro/src/store/counterReducer.jsx
@@ -1,1 +1,17 @@
-export const counterReducer = (state, action) => {}
+const initialState = {
+  count: 0,
+};
+
+export const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INC":
+      return { count: state.count + 1 };
+    case "DEC":
+      return { count: state.count - 1 };
+    case "RESET":
+      return { count: 0 };
+
+    default:
+      return state
+  }
+};
