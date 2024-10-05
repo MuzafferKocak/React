70239e4f128960d7ec700271b37a653eede63d19
Mahir <mkojak75@gmail.com>
2024-10-05 23:48:03 +0200

    f

--- 13_redux-intro/src/components/counter/Counter.jsx
+++ 13_redux-intro/src/components/counter/Counter.jsx
@@ -1,12 +1,19 @@
 import { useSelector, useDispatch } from "react-redux";
 import "./Counter.css";
-import { dec, decrease, inc, increase, res, reset } from "../../store/counterReducer";
+import {
+  dec,
+  decrease,
+  inc,
+  increase,
+  res,
+  reset,
+} from "../../store/counterReducer";
 
 const Counter = () => {
   //? Der useSelector-Hook wird zum Lesen von Daten aus dem globalen State verwendet
 
-//   const count = useSelector((state) => state.counter.count);
-  const {count} = useSelector((state) => state.counter);
+  //   const count = useSelector((state) => state.counter.count);
+  const { count } = useSelector((state) => state.counter);
 
   const dispatch = useDispatch();
   console.log(count);
@@ -18,8 +25,8 @@ const Counter = () => {
       <div>
         <button
           className="counter-button positive"
-        //   onClick={() => dispatch({ type: "INC" })}
-        //   onClick={() => dispatch({ type: inc })}
+          //   onClick={() => dispatch({ type: "INC" })}
+          //   onClick={() => dispatch({ type: inc })}
           onClick={() => dispatch(increase())}
         >
           increase
@@ -27,8 +34,8 @@ const Counter = () => {
 
         <button
           className="counter-button zero"
-        //   onClick={() => dispatch({ type: "RESET" })}
-        //   onClick={() => dispatch({ type: reset })}
+          //   onClick={() => dispatch({ type: "RESET" })}
+          //   onClick={() => dispatch({ type: reset })}
           onClick={() => dispatch(res())}
         >
           reset
@@ -36,8 +43,8 @@ const Counter = () => {
 
         <button
           className="counter-button negative"
-        //   onClick={() => dispatch({ type: "DEC" })}
-        //   onClick={() => dispatch({ type: dec })}
+          //   onClick={() => dispatch({ type: "DEC" })}
+          //   onClick={() => dispatch({ type: dec })}
           onClick={() => dispatch(decrease())}
         >
           decrease
