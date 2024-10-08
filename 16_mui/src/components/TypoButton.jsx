d3a139ac57b0bb065d306c084c04e90dee488d5b
Mahir <mkojak75@gmail.com>
2024-10-08 20:36:07 +0200

    starter

---
+++ 16_mui/src/components/TypoButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TypoButton = () => {
+  return (
+    <div>TypoButton</div>
+  )
+}
+
+export default TypoButton
---
+++ 16_mui/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import TypoButtons from "../components/TypoButtons";
+
+const Home = () => {
+  return (
+    <>
+      <TypoButtons />
+    </>
+  );
+};
+
+export default Home;
