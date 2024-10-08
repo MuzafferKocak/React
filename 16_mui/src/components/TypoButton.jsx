d2d297f37308e4e48997f20cd7748b9c772d8ba1
Mahir <mkojak75@gmail.com>
2024-10-08 20:59:25 +0200

    mui

--- 16_mui/src/components/TypoButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TypoButton = () => {
-  return (
-    <div>TypoButton</div>
-  )
-}
-
-export default TypoButton
---
+++ 16_mui/src/components/TypoButtons.jsx
@@ -0,0 +1,14 @@
+// import Typography from '@mui/material/Typography';
+// or
+import { Typography } from '@mui/material';
+
+
+
+const TypoButton = () => {
+  return (
+    <Typography>
+    </Typography>
+  )
+}
+
+export default TypoButton
