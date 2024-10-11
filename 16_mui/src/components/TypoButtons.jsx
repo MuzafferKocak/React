5b849db10a3a32242ebb2f917d818c8a11c6e5ad
Mahir <mkojak75@gmail.com>
2024-10-11 17:31:49 +0200

    buttons

--- 16_mui/src/components/TypoButtons.jsx
+++ 16_mui/src/components/TypoButtons.jsx
@@ -2,26 +2,56 @@ import Typography from "@mui/material/Typography";
 // or
 //? named import
 // import { Typography } from '@mui/material';
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 const TypoButton = () => {
   return (
     <>
-      <Typography variant="h2" component="h3">
-        Beispiel
-      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-end",
+          p: 2,
+          mt: 3,
+        }}
+      >
+        <Typography variant="h2" component="h3">
+          Beispiel
+        </Typography>
 
-      <Typography variant="body1" color="violet">
-        Beispiel Element
+        <Typography variant="body1" color="violet">
+          Beispiel Element
+        </Typography>
+        <Typography variant="body2" color="#dc143c">
+          Beispiel Element
+        </Typography>
+        <Typography variant="subtitle1" color="primary">
+          Beispiel Element
+        </Typography>
+        <Typography variant="subtitle2" color="secondary" mt={2}>
+          Beispiel Element
+        </Typography>
+        <Typography variant="button" color="error" mt={2}>
+          Beispiel Element
+        </Typography>
+      </Box>
+      <Typography variant="h2" align="center" color="turquoise" mt={4} flexWrap={"wrap"}>
+        BUTTONS
       </Typography>
-      <Typography variant="body2" color="#dc143c">
-        Beispiel Element
-      </Typography>
-      <Typography variant="subtitle1" color="primary">
-        Beispiel Element
-      </Typography>
-      <Typography variant="subtitle2" color="secondary">
-        Beispiel Element
-      </Typography>
+      <Stack direction={"row"} mt={3} gap={2} justifyContent={"center"}>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined" color="error">Outlined</Button>
+        <Button variant="contained" color="warning">Outlined</Button>
+        <Button variant="contained" sx={{
+          color: "black",
+          backgroundColor: "#bede",
+          ":hover": {backgroundColor: "#3ac798ed"}
+        }}>Outlined</Button>
+      </Stack>
     </>
   );
 };
