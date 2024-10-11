b93334d0cfbefa9797689160ac1eb2ca06fe763a
Mahir <mkojak75@gmail.com>
2024-10-11 17:12:49 +0200

    variant

--- 16_mui/src/components/TypoButtons.jsx
+++ 16_mui/src/components/TypoButtons.jsx
@@ -1,14 +1,29 @@
-// import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 // or
-import { Typography } from '@mui/material';
-
-
+//? named import
+// import { Typography } from '@mui/material';
 
 const TypoButton = () => {
   return (
-    <Typography>
-    </Typography>
-  )
-}
+    <>
+      <Typography variant="h2" component="h3">
+        Beispiel
+      </Typography>
 
-export default TypoButton
+      <Typography variant="body1" color="violet">
+        Beispiel Element
+      </Typography>
+      <Typography variant="body2" color="#dc143c">
+        Beispiel Element
+      </Typography>
+      <Typography variant="subtitle1" color="primary">
+        Beispiel Element
+      </Typography>
+      <Typography variant="subtitle2" color="secondary">
+        Beispiel Element
+      </Typography>
+    </>
+  );
+};
+
+export default TypoButton;
